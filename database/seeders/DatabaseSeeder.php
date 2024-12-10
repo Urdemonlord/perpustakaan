001898bb2263a7634941eed2a015c1192809e374
Urdemonlord <hasrinata@gmail.com>
2024-12-11 01:01:42 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   config/database.php
    	modified:   database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\AccountSeeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AccountSeeder::class
+        ]);
+
+        // Hapus data yang ada
+        DB::table('users')->truncate();
+        
+        // Insert users
+        DB::table('users')->insert([
+            [
+                'nama' => 'admin',
+                'username' => 'admin',
+                'password' => md5('admin123'),
+                'role' => 'admin'
+            ],
+            [
+                'nama' => 'user',
+                'username' => 'user',
+                'password' => md5('user123'),
+                'role' => 'user'
+            ]
         ]);
     }
 }
